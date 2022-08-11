18997bf1ea8452b1a2d0344c540db7c09ae55f12
Reingalrt <adorufus@gmail.com>
2022-08-11 14:46:08 +0700

    commit

--- api/controllers/banner/getBannerController.js
+++ api/controllers/banner/getBannerController.js
@@ -1,197 +1,191 @@
-const mongoose = require("mongoose");
-const { storage } = require("firebase-admin");
-const Banner = mongoose.model("Banner");
-const image_kit = require("imagekit");
-const cloudinary = require("cloudinary");
-const { sample } = require("lodash");
+const mongoose = require('mongoose')
+const { storage } = require('firebase-admin')
+const Banner = mongoose.model('Banner')
+const image_kit = require('imagekit')
+const cloudinary = require('cloudinary')
+const { sample } = require('lodash')
+
+var baseFileUrl = 'https://103.163.139.152:8080/files/'
 
 module.exports.getAllBanner = (req, res) => {
   Banner.find({}, (err, banners) => {
     if (err) {
       return res.status(400).json({
-        status: "error",
+        status: 'error',
         error: err,
-      });
+      })
     } else {
       return res.status(200).json({
-        status: "success",
-        message: "Banner Found",
+        status: 'success',
+        message: 'Banner Found',
         data: banners,
-      });
+      })
     }
-  });
-};
+  })
+}
 
 module.exports.getBanner = (req, res) => {
-  var id = req.query.id;
+  var id = req.query.id
 
   Banner.findById(id, (err, banner) => {
     if (err) {
       return res.status(400).json({
-        status: "error",
+        status: 'error',
         error: err,
-      });
+      })
     } else {
       if (banner) {
         return res.status(200).json({
-          status: "success",
-          message: "Banner found!",
+          status: 'success',
+          message: 'Banner found!',
           data: banner,
-        });
+        })
       } else {
         return res.status(400).json({
-          status: "error",
-          error: "Banner not found",
-        });
+          status: 'error',
+          error: 'Banner not found',
+        })
       }
     }
-  });
-};
+  })
+}
 
 module.exports.editBanner = async (req, res) => {
-  var { banner_id, banner_name, go_to_url } = req.body;
+  var { banner_id, banner_name, go_to_url } = req.body
+
+  var file = req.file
 
   if (banner_id) {
     Banner.findById(banner_id, (err, banner) => {
       if (err) {
         return res.status(400).json({
-          status: "error",
+          status: 'error',
           error: err,
-        });
+        })
       } else {
         if (banner) {
-          updateImageBucket(req.file, banner["metaname"], res).then((value) => {
-            console.log(value);
-            // const bannerModel = new Banner();
-            // bannerModel.bannerName = banner_name;
-            // bannerModel.url = go_to_url;
-            // bannerModel.metaname = value["public_id"];
-            // bannerModel.bannerFile = value["secure_url"];
-
-            Banner.findByIdAndUpdate(
-              banner_id,
-              {
-                bannerName: banner_name,
-                url: go_to_url,
-                metaname: value["public_id"],
-                bannerFile: value["secure_url"],
-              },
-              (err, data) => {
-                if (err) {
-                  return res.status(400).json({
-                    status: "failed",
-                    error: err,
-                  });
-                } else {
-                  return res.status(200).json({
-                    status: "success",
-                    message: "Banner Updated",
-                    data: data,
-                  });
-                }
+          Banner.findByIdAndUpdate(
+            banner_id,
+            {
+              bannerName: banner_name ? banner_name : banner.bannerName,
+              url: go_to_url ? go_to_url : banner.url,
+              bannerFile: file ? baseFileUrl + file.filename : banner.bannerFile,
+            },
+            (err, data) => {
+              if (err) {
+                return res.status(400).json({
+                  status: 'failed',
+                  error: err,
+                })
+              } else {
+                return res.status(200).json({
+                  status: 'success',
+                  message: 'Banner Updated',
+                  data: data,
+                })
               }
-            );
-          });
+            },
+          )
         } else {
           return res.status(400).json({
-            status: "error",
-            error: "Banner not found",
-          });
+            status: 'error',
+            error: 'Banner not found',
+          })
         }
       }
-    });
+    })
   } else {
     return res.status(400).json({
-      status: "failed",
-      message: "field: Id is required!",
-    });
+      status: 'failed',
+      message: 'field: Id is required!',
+    })
   }
-};
+}
 
 module.exports.deleteBannerController = async (req, res) => {
-  const banner_id = req.query.banner_id;
+  const banner_id = req.query.banner_id
 
   Banner.findById(banner_id, async (err, banner) => {
-    console.log("finding....");
+    console.log('finding....')
     if (err) {
-      console.log(err);
+      console.log(err)
       return res.status(400).json({
-        status: "failed!",
+        status: 'failed!',
         error: err,
-      });
+      })
     } else {
       if (banner) {
-        console.log(banner);
-        await deleteBanner(banner["metaname"], res).then((value) => {
-          if (value == "deleted") {
+        console.log(banner)
+        await deleteBanner(banner['metaname'], res).then((value) => {
+          if (value == 'deleted') {
             Banner.findByIdAndDelete(banner_id, (err, banner) => {
               if (err) {
                 return res.status(400).json({
-                  status: "failed",
+                  status: 'failed',
                   error: err,
-                });
+                })
               } else {
                 return res.status(200).json({
-                  status: "success",
-                  message: "Image deleted.",
-                });
+                  status: 'success',
+                  message: 'Image deleted.',
+                })
               }
-            });
+            })
           } else {
             return res.status(400).json({
-              status: "error",
-              error: "Banner not found",
-            });
+              status: 'error',
+              error: 'Banner not found',
+            })
           }
-        });
+        })
       } else {
         return res.status(400).json({
-          status: "error",
-          error: "Banner not found"
+          status: 'error',
+          error: 'Banner not found',
         })
       }
     }
-  });
-};
+  })
+}
 
 const updateImageBucket = async (file, metaname, res) => {
-  var data;
+  var data
 
-  await deleteBanner(metaname, res);
+  await deleteBanner(metaname, res)
 
   await cloudinary.v2.uploader.upload(file.path, (err, uploadResult) => {
     if (err) {
       return res.status(400).json({
-        status: "error",
-        type: "cloudinary upload",
+        status: 'error',
+        type: 'cloudinary upload',
         error: err,
-      });
+      })
     } else {
       data = {
         public_id: uploadResult.public_id,
         secure_url: uploadResult.secure_url,
-      };
+      }
     }
-  });
+  })
 
-  return data;
-};
+  return data
+}
 
 const deleteBanner = async (metaname, res) => {
-  var data;
+  var data
   await cloudinary.v2.uploader.destroy(metaname, async (err, result) => {
     if (err) {
-      console.log("Cloudinary error: " + err);
+      console.log('Cloudinary error: ' + err)
       return res.status(400).json({
-        status: "error",
-        type: "cloudinary delete",
+        status: 'error',
+        type: 'cloudinary delete',
         error: err,
-      });
+      })
     } else {
-      console.log("Cloudinary response: " + JSON.stringify(result));
-      data = "deleted";
+      console.log('Cloudinary response: ' + JSON.stringify(result))
+      data = 'deleted'
     }
-  });
+  })
 
-  return data;
-};
+  return data
+}
